4de91a41ef9cfae8e40d567d9407747d5ce60c5d
satryaputra <me.satryaputra@gmail.com>
2026-03-27 15:42:37 +0700

    chore: update README, LICENSE, next config, and skills lock

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    qualities: [25, 50, 75, 90],
+  },
 };
 
 export default nextConfig;
